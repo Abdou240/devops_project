b89f2520658e2da942b96b6bf3015e7e6c29a439
khaledg4g <mouhamedkhaled.baoueb@esprit.tn>
2025-01-04 21:24:22 +0100

    deploy

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,8 +14,8 @@ provider "aws" {
   region = var.region
 }
 resource "aws_instance" "servernode" {
-  ami                    = "ami-075449515af5df0d1"
-  instance_type          = "t2.micro"
+  ami                    = "ami-089146c5626baa6bf"
+  instance_type          = "t3.micro"
   key_name               = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.maingroup.id]
   iam_instance_profile   = aws_iam_instance_profile.ec2-profile.name
