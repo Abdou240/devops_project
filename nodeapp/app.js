c82eb2b6791c6c7c8144de19a194269917555c42
khaledg4g <mouhamedkhaled.baoueb@esprit.tn>
2025-01-06 17:40:06 +0100

    Reverted changes to commit d8cb8dd040bd0eeeabef1b5fa4b23db7f9a64e45

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -62,6 +62,7 @@ app.get("/", (req, res) => {
         <span>OWASP ZAP</span>
         <span>Nexus</span>
         <span>Java</span>
+      
         <span>React.js</span>
         <span>Laravel</span>
         <span>Angular</span>
